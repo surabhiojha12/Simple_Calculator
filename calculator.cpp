0d8ebf2edc6e43333c2ae04d145b686d335d99ba
SURABHI OJHA <surabhiojha12@gmail.com>
2020-08-14 20:06:14 +0530

    Added calculator templates

---
+++ calculator.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int addition(int number_1, int number_2){
+
+}
+
+int subtraction(int number_1, int number_2){
+
+}
+
+int multiplication(int number_1, int number_2){
+
+}
+
+int division(int number_1, int number_2){
+
+}
+
+int get_input(){
+
+}
+
+int main(){
+    get_input();
+    return 0;
+}
---
+++ calculator.py
@@ -0,0 +1,11 @@
+def addition(number_1, number_2):
+
+def subtraction(number_1, number_2):
+
+def multiplication(number_1, number_2):
+
+def division(number_1, number_2):
+
+def get_input():
+
+input()
