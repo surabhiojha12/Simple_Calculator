af382e4167166e98eda174a239605e76453266d3
jeevana246 <69810945+jeevana246@users.noreply.github.com>
2020-08-18 23:17:38 +0530

    Updated subtraction function.

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@ def addition(number_1, number_2):
   print(number_1,"+",number_2,"=",(number_1+number_2))
 
 def subtraction(number_1, number_2):
-
+  print(number_1,"-",number_2,"=",(number_1-number_2))
 
 def multiplication(number_1, number_2):
 
@@ -14,4 +14,6 @@ def get_input():
   c=str(input())
   if(c=="+"):
     addition(a,b)
+  elif(c=="-"):
+    subtraction(a,b)
 get_input()
