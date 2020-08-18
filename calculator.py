d8f70c0d73fa4d389723e40e8ec6e4a25970f1be
jeevamutharasi <69708900+jeevamutharasi@users.noreply.github.com>
2020-08-19 00:15:17 +0530

    updated get_input

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 def addition(number_1, number_2):
+  return 
 
 def subtraction(number_1, number_2):
 
@@ -7,5 +8,18 @@ def multiplication(number_1, number_2):
 def division(number_1, number_2):
 
 def get_input():
+  print("Enter num1 operator num2")
+  num1=input(int)
+  op=input(char)
+  numb2=input(int)
+  if(op=='+')
+  addition(num1,num2)
+  elif(op=='-')
+  subtraction(num1,num2)
+  elif(op=='*')
+  multiplication(num1,num2)
+  elif(op=='/')
+  division(num1,num2)
+  
 
 input()
