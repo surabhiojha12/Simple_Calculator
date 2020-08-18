b0cbf9c6631d2ad263a4ad2b20b6c27156d2f69e
jeevana246 <69810945+jeevana246@users.noreply.github.com>
2020-08-19 00:31:59 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,4 @@
 def addition(number_1, number_2):
-<<<<<<< Updated upstream
-    return number_1+number_2
-def subtraction(number_1, number_2):
-    return number_1-number_2
-def multiplication(number_1, number_2):
-    return number_1*number_2
-def division(number_1, number_2):
-    return number_1/number_2
-def get_input():
-    num1, operate, num2 = input().split()
-    num1=int(num1)
-    num2=int(num2)
-    if(operate=='+'):
-        print(addition(num1,num2))
-    if(operate=='-'):
-        print(subtraction(num1,num2))
-    if(operate=='*'):
-        print(multiplication(num1,num2))
-    if(operate=='/'):
-        print(division(num1, num2))
-get_input()
-=======
     print(number_1,"+",number_2,"=",(number_1+number_2))
 
 def subtraction(number_1, number_2):
@@ -45,4 +23,4 @@ def get_input():
     elif(c=="/"):
         division(a,b)
 get_input()
->>>>>>> Stashed changes
+
