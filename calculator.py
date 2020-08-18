0f57e9d0031df5b678896493dec5cdd4fdeee4ec
Jeevitha38 <69860781+Jeevitha38@users.noreply.github.com>
2020-08-18 22:41:08 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,21 @@
 def addition(number_1, number_2):
-
+    return number_1+number_2
 def subtraction(number_1, number_2):
-
+    return number_1-number_2
 def multiplication(number_1, number_2):
-
+    return number_1*number_2
 def division(number_1, number_2):
-
+    return number_1/number_2
 def get_input():
-
-input()
+    num1, operate, num2 = input().split()
+    num1=int(num1)
+    num2=int(num2)
+    if(operate=='+'):
+        print(addition(num1,num2))
+    if(operate=='-'):
+        print(subtraction(num1,num2))
+    if(operate=='*'):
+        print(multiplication(num1,num2))
+    if(operate=='/'):
+        print(division(num1, num2))
+get_input()
