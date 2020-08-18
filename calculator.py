bcf18af170d6de60b6e3cb2e40ef6f356c059163
jeevamutharasi <69708900+jeevamutharasi@users.noreply.github.com>
2020-08-19 00:21:17 +0530

    updated definitions

--- calculator.py
+++ calculator.py
@@ -1,24 +1,27 @@
 def addition(number_1, number_2):
-  return 
+  print("=",number_1+number_2)
 
 def subtraction(number_1, number_2):
+   print("=",number_1-number_2)
 
 def multiplication(number_1, number_2):
+   print("=",number_1*number_2)
 
 def division(number_1, number_2):
+   print("=",number_1/number_2)
 
 def get_input():
   print("Enter num1 operator num2")
   num1=input(int)
   op=input(char)
   numb2=input(int)
-  if(op=='+')
+  if(op=='+'):
   addition(num1,num2)
-  elif(op=='-')
+  elif(op=='-'):
   subtraction(num1,num2)
-  elif(op=='*')
+  elif(op=='*'):
   multiplication(num1,num2)
-  elif(op=='/')
+  elif(op=='/'):
   division(num1,num2)
   
 
