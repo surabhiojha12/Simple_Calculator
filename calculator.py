b63fbf1cc2e10a5e4ab528da7d7b4a3c9e1b4326
jeevana246 <69810945+jeevana246@users.noreply.github.com>
2020-08-18 23:15:51 +0530

    Updated the definitions of functions
    Added addition function with input function.

--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
 def addition(number_1, number_2):
+  print(number_1,"+",number_2,"=",(number_1+number_2))
 
 def subtraction(number_1, number_2):
+
 
 def multiplication(number_1, number_2):
 
@@ -9,5 +11,7 @@ def division(number_1, number_2):
 def get_input():
   a=int(input())
   b=int(input())
-
-input()
+  c=str(input())
+  if(c=="+"):
+    addition(a,b)
+get_input()
