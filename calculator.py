7342f61823d9254a66dcd85c203a1371b77a7284
jeevana246 <69810945+jeevana246@users.noreply.github.com>
2020-08-18 23:38:13 +0530

    Updated all functions with definitions.

--- calculator.py
+++ calculator.py
@@ -1,13 +1,26 @@
 def addition(number_1, number_2):
+  print(number_1,"+",number_2,"=",(number_1+number_2))
 
 def subtraction(number_1, number_2):
+  print(number_1,"-",number_2,"=",(number_1-number_2))
 
 def multiplication(number_1, number_2):
-
+  print(number_1,"*",number_2,"=",(number_1*number_2))
+  
 def division(number_1, number_2):
+  print(number_1,"/",number_2,"=",(number_1/number_2))
 
 def get_input():
   a=int(input())
   b=int(input())
+  c=str(input())
+  if(c=="+"): 
+    addition(a,b)
+  elif(c=="-"):
+    subtraction(a,b)
+  elif(c=="*"):
+    multiplication(a,b)
+  elif(c=="/"):
+    division(a,b)
 
-input()
+get_input()
