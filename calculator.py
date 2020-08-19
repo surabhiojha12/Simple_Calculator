28ff48f288dfb92d92b2f56009be7283ca5853af
suganthiM2306 <69864460+suganthiM2306@users.noreply.github.com>
2020-08-19 20:36:32 +0530

    Operation_selection
    selecting operation to perform

--- calculator.py
+++ calculator.py
@@ -7,5 +7,11 @@ def multiplication(number_1, number_2):
 def division(number_1, number_2):
 
 def get_input():
+#select option
+
+print("***SIMPLE CALCULATOR***")
+print("Select Operation \n 1.Addition \n 2.Subtraction \n 3.Multiplication \n 4.Division")
+
+opt=int(input("operation : "))
 
 input()
