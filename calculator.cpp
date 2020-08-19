e3d7c22d37dcba0ff4db5d8556b1837249ce09ad
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2020-08-19 21:52:19 +0530

    Update calculator.cpp
    Added function definition for addition, subtraction, multiplication and division and also added function definition for get_input() and used switch case for each operation performed and printed the result on the same

--- calculator.cpp
+++ calculator.cpp
@@ -2,23 +2,46 @@
 using namespace std;
 
 int addition(int number_1, int number_2){
+return number_1+number_2;
 
 }
 
 int subtraction(int number_1, int number_2){
 
+return number_1-number_2;
 }
 
 int multiplication(int number_1, int number_2){
-
+return number_1*number_2;
 }
 
 int division(int number_1, int number_2){
 
+return number_1/number_2;
 }
 
 int get_input(){
-
+int ch,a,b;
+cout<<"enter a choice";
+cin>>ch;
+switch(ch)
+{
+ case 1:{ cin>>a>>b;
+          cout<<addition(a,b);
+          break;
+        }
+ case 2:{ cin>>a>>b;
+          cout<<subtraction(a,b);
+          break;
+        }
+ case 3:{ cin>>a>>b;
+          cout<<multiplication(a,b);
+          break;
+        }
+ case 4:{ cin>>a>>b;
+          cout<<division(a,b);
+          break;
+        }
 }
 
 int main(){
