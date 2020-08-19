771ca3229bf2bda430915f7308edab6e6506b614
vishalmighty <69051998+vishalmighty@users.noreply.github.com>
2020-08-19 18:57:00 +0530

    Selecting a choice

--- calculator.py
+++ calculator.py
@@ -10,6 +10,7 @@ def multiplication(number_1, number_2):
 def division(number_1, number_2):
             print("Quotient",number_1 // number_2)
 
-def get_input():
-
-input()
+print("\t\t******Simple Calculator******")
+print("1 sum\t2 subtract\t3 multiply \t4 divide")
+print("Enter the option you want to perform:")
+n=int(input())
