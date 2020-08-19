6ed0459d8b6afd828de37c34fb7679d62dcc2f6d
supriya073 <69890732+supriya073@users.noreply.github.com>
2020-08-19 20:45:41 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -13,4 +13,16 @@ print("1.addition 2.subtraction 3.multiplication 4.division")
 ch=input("enter choice:")
 number1=float(input("enter number 1:"))
 number2=float(input("enter number 2:"))
+if ch== "1":
+    print(number_1+number_2)
+
+elif ch=="2":
+    print(number_1-number_2)
+elif ch=="3":
+    print(number_1*number_2)
+elif ch=="4":
+    print(number_1/number_2)
+else:
+    print("Invalid input")
+    
 
