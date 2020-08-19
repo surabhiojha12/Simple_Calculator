06701bbedb31eed2c2f4c7428f562b90cc2bd404
sujithapradeep <69863460+sujithapradeep@users.noreply.github.com>
2020-08-19 19:42:08 +0530

    function call

--- calculator.py
+++ calculator.py
@@ -17,5 +17,11 @@ def division(number_1, number_2):
 number_1 = int(input("enter first number: "))
 number_2 = int(input("enter second number: "))
 print("1 - add\t 2 - sub\t 3 - multiply\t 4 - divide")
+a = int(input("enter the option: "))
+if(a == 1):
+    addition(number_1,number_2)
+elif(a == 2):
+    subtraction(number_1,number_2)
+
 
 
