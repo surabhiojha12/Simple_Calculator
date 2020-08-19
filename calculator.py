ebd62da5e70dd2b4520a0b76dff8915332fa5de0
sujithapradeep <69863460+sujithapradeep@users.noreply.github.com>
2020-08-19 20:23:52 +0530

    choosing an option

--- calculator.py
+++ calculator.py
@@ -10,10 +10,5 @@ def multiplication(number_1, number_2):
 def division(number_1, number_2):
     print("quotient of", number_1, " and ", number_2, " is ", (number_1 // number_2))
 
-
-
-
-
-def get_input():
-
-input()
+print("1.ADDITION\n2.SUBTRACTION\n3.MULTIPLICATION\n4.DIVISION")
+n = int(input("enter the option: "))
