76ac7c9d35937f3b5fd927425d49aeced239c76c
sujithapradeep <69863460+sujithapradeep@users.noreply.github.com>
2020-08-19 21:22:48 +0530

    defining multiplication and division

--- calculator.py
+++ calculator.py
@@ -6,9 +6,14 @@ def subtraction(number_1, number_2):
 
 
 def multiplication(number_1, number_2):
+    print("product of",number_1," and ",number_2," is ",(number_1 * number_2))  
 
 def division(number_1, number_2):
+    print("quotient of",number_1," and ",number_2," is ",(number_1 // number_2))  
 
 def get_input():
+
+
+
 
 input()
