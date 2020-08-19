2909edf73a3842dde23f5e97dc35f3e2461e9e33
supriya073 <69890732+supriya073@users.noreply.github.com>
2020-08-19 20:34:15 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,6 +6,11 @@ def multiplication(number_1, number_2):
 
 def division(number_1, number_2):
 
-def get_input():
 
-input()
+
+print("1.addition 2.subtraction 3.multiplication 4.division")
+
+ch=input("enter choice:")
+number1=float(input("enter number 1:"))
+number2=float(input("enter number 2:"))
+
