49a276f642680cbfb8461d45573cebcbdf601ec4
vishalmighty <69051998+vishalmighty@users.noreply.github.com>
2020-08-19 18:53:37 +0530

    Defined Multiplication and Division

--- calculator.py
+++ calculator.py
@@ -5,8 +5,10 @@ def subtraction(number_1, number_2):
             print(number_1 - number_2)
 
 def multiplication(number_1, number_2):
+            print("Product",number_1 * number_2)
 
 def division(number_1, number_2):
+            print("Quotient",number_1 // number_2)
 
 def get_input():
 
