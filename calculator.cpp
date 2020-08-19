8503a8fb97beeba978b2b7524223fd875939202d
Thuhin Khanna <qor6119@gmail.com>
2020-08-19 11:14:10 +0530

    Update Division Function in calculator.cpp
    Completed Definition of One Outlined function
    
    Definition of the function Division has been updated along with appropriate changes in get_input()

--- calculator.cpp
+++ calculator.cpp
@@ -14,7 +14,7 @@ return number_1*number_2;
 }
 
 int division(int number_1, int number_2){
-
+return number_1/number_2;
 }
 
 int get_input(){
@@ -24,6 +24,7 @@ cin>>a>>b;
 cout<<"\n Sum of Numbers : "<<addition(a,b);
 cout<<"\n Difference of Numbers : "<<subtraction(a,b);
 cout<<"\n Product of Numbers : "<<multiplication(a,b);
+cout<<"\n Quotient of Division of Numbers : "<<division(a,b);
 }
 
 int main(){
