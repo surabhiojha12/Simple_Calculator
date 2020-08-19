34fb3b975139fd8fc6fdf9f73bf503258372323a
sujithapradeep <69863460+sujithapradeep@users.noreply.github.com>
2020-08-19 21:34:58 +0530

    function call

--- calculator.py
+++ calculator.py
@@ -19,7 +19,18 @@ def get_input():
 
 print("1.ADDITION\n2.SUBTRACTION\n3.MULTIPLICATION\n4.DIVISION")
 n = int(input("enter the option: "))
-
-
-
 get_input()
+if(n == 1):
+    addition(a,b)
+elif(n == 2):
+    subtraction(a,b)
+elif(n == 3):
+    multiplication(a,b)
+elif(n == 4):
+    division(a,b)
+else:
+    print("INVALID OPTION")
+
+
+
+
