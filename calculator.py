9abbb3582072294b428ca0fde7eb41e64c22157a
anagha157 <69710957+anagha157@users.noreply.github.com>
2020-08-19 20:19:04 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -7,5 +7,18 @@ def multiplication(number_1, number_2):
 def division(number_1, number_2):
 
 def get_input():
+  a,b,c = input().split()
+  a=int(a)
+  c=int(c)
+  if(b == '+'):
+    addition(a,c)
+  elif(b == '-'):
+    subtraction(a,c)
+  elif(b == '*'):
+    multiplication(a,c)
+  elif(b == '/'):
+    division(a,c)
+  else:
+    print("invalid input")
 
-input()
+get_input()
