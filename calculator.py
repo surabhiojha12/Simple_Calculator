14f15c1d4ca4a4db2d788ecf649556878b4e04ee
Akhash16 <akhash.s.2019.cse@rajalakshmi.edu.in>
2020-08-20 00:23:05 +0530

    190701010_calculator_py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,23 @@
 def addition(number_1, number_2):
+    print('Addition:',number_1 + number_2)
 
 def subtraction(number_1, number_2):
+     print('Subtraction:',number_1 - number_2)
 
 def multiplication(number_1, number_2):
+     print('Multiplication:',number_1 * number_2)
 
 def division(number_1, number_2):
+     print('Division:',number_1 / number_2)
 
 def get_input():
+    global x, y
+    x = int(input('Enter num1: '))
+    y = int(input('Enter num2: '))
 
-input()
+    
+get_input()
+addition(x, y)
+subtraction(x, y)
+multiplication(x, y)
+division(x, y)
