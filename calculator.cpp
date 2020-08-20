1a46d3a8590d03867c71fdc2fd0017d151a37aac
vikikrish183g <vikramkrishna.s.2019.cse@rajalakshmi.edu.in>
2020-08-20 20:21:58 +0530

    updated calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -2,23 +2,39 @@
 using namespace std;
 
 int addition(int number_1, int number_2){
-
 }
 
 int subtraction(int number_1, int number_2){
-
+	return number_1-number_2;
 }
 
 int multiplication(int number_1, int number_2){
-
+	return number_1*number_2;
 }
 
 int division(int number_1, int number_2){
-
+	if(number_2==0)
+	{
+		cout<<"division by zero not possible";
+		return 0;
+	}
+	else
+		return number_1/number_2;
 }
 
-int get_input(){
-
+int get_input()
+{
+	int a,b;
+	cout<<"ENTER TWO NUMBERS";
+	cin>>a>>b;
+	
+	cout<<"addition of"<<a<<"and"<<b<<":"<<addition(a,b)<<"\n";
+	cout<<"subraction of"<<a<<"and"<<b<<":"<<subraction(a,b)<<"\n";
+	cout<<"multiplication of"<<a<<"and"<<b<<":"<<multiplication(a,b)<<"\n";
+	cout<<"division of"<<a<<"and"<<b<<":"<<division(a,b);	
+	
+	return 0;
+	
 }
 
 int main(){
