8a52d515000c9b7e411427a06dce95ba92b22dd2
suganthiM2306 <69864460+suganthiM2306@users.noreply.github.com>
2020-08-20 15:56:25 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -8,10 +8,12 @@ def multiplication(number_1, number_2):
     return (number_1*number_2)
 
 def division(number_1, number_2):
-    return(number_1/number_2)
+    try:
+        return(number_1/number_2)
+    except ZeroDivisionError:
+        return 0
+    
 
-
-#select option
 
 print("***SIMPLE CALCULATOR***")
 print("Select Operation \n 1.Addition \n 2.Subtraction \n 3.Multiplication \n 4.Division")
@@ -20,25 +22,24 @@ opt=int(input("operation : "))
 
 Number1=int(input("Enter the First Number = "))
 Number2=int(input("Enter the Second Number = "))
-int result
+
 
 if opt == 1:
-     result=addition(Number1,Number2)
-     print("Addition of ",Number1, " and",Number2 " = "result)
+     addition(Number1,Number2)
+     print("Addition of ",Number1, " and",Number2 ," = ",addition(Number1,Number2))
      
-else if opt == 2:
-     result=subtraction(Number1,Number2)
-     print("Subtraction of ",Number1, " and",Number2 " = "result)
+elif opt == 2:
+     print("Subtraction of ",Number1, " and",Number2, " = ", subtraction(Number1,Number2))
      
      
-else if opt == 3:
-    result=multiplication(Number1,Number2)
-    print("Multiplication of ",Number1, " and",Number2 " = "result)
+elif opt == 3:
+
+    print("Multiplication of ",Number1, " and",Number2 ," = ", multiplication(Number1,Number2))
     
     
-else if opt == 4:
-    result=division(Number1,Number2)
-    print("Division of ",Number1, " and",Number2 " = "result)
+elif opt == 4:
+   
+    print("Division of ",Number1, " and",Number2, " = ", division(Number1,Number2))
     
 else:
     print("Invalid input")
