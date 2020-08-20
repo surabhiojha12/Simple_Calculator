a44eed2474a9cbe77e878bbb3cc4d8d492189599
daadreyaa30 <69893232+daadreyaa30@users.noreply.github.com>
2020-08-20 21:01:48 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,19 @@
 def addition(number_1, number_2):
+  return number_1 + number_2
 
 def subtraction(number_1, number_2):
+  return number_1 - number_2
 
 def multiplication(number_1, number_2):
+  return number_1 * number_2
 
 def division(number_1, number_2):
+  return nuber_1 / number_2
 
 def get_input():
+  number_1 = int(input())
+  number_2 = int(input())
+ 
+get_input()
 
-input()
+
