644420b86d742a15a29dec7cf594a7fbcc25bd15
Jayapriyaa <69856741+Jayapriyaa@users.noreply.github.com>
2020-08-20 20:36:37 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,13 +1,16 @@
 def addition(number_1, number_2):
-  print(number_1,"+",number_2,"=",(number_1+number_2))
+  result=number_1+number_2
+  print(number_1,"+",number_2,"=",result)
 def subtraction(number_1, number_2):
-  print(number_1,"-",number_2,"=",(number_1-number_2))
+  result=number_1-number_2
+  print(number_1,"-",number_2,"=", result)
 
 def multiplication(number_1, number_2):
-  print(number_1,"*",number_2,"=",(number_1*number_2))
+  result=number_1*number_2
+  print(number_1,"*",number_2,"=", result)
 
 def division(number_1, number_2):
-  print(number_1,"/",number_2,"=",(number_1/number_2))
+  return(number_1/number_2)
 
 def get_input():
   number_1=int(input())
@@ -20,6 +23,12 @@ def get_input():
   elif(operator=="*"):
     multiplication(number_1,number_2)
   elif(operator=="/"):
-    division(number_1,number_2)
+    try:
+      result=division(number_1,number_2)
+      print(number_1,"/", number_2,"=",result)
+    except ZeroDivisonError as error:
+      print(error) 
+      
+    
 
 get_input()
