ffd323e4e76cfafb81222d8fe21c52b7072fdd49
suganthiM2306 <69864460+suganthiM2306@users.noreply.github.com>
2020-08-20 14:45:44 +0530

    performing calculations

--- calculator.py
+++ calculator.py
@@ -1,10 +1,14 @@
 def addition(number_1, number_2):
+    return(number_1+ number_2)
 
 def subtraction(number_1, number_2):
+    return(number_1 - number_2)
 
 def multiplication(number_1, number_2):
+    return(number_1 * number_2)
 
 def division(number_1, number_2):
+    return(number_1 // number_2)
 
 print("SIMPLE CALCULATOR")
 number_1=int(input("Enter First Number"))
@@ -16,12 +20,17 @@ print("Select option: 1.Addition \n 2.Subtraction \n 3.Multiplication \n 4.Divis
 option=int(input("your option : "))
 
 if(option==1):
+    addition(number_1, number_2)
     
 elif(option==2):
+    subtraction(number_1, number_2)
 
 elif(option==3):
+    multiplication(number_1, number_2)
+
 
 elif(option==4):
+    division(number_1, number_2)
     
 else:
     print("Invalid Input")
