ccfec0c842ceb6ffcc378bb6f1fc6bead5dfe3f1
Urjha24 <69858525+Urjha24@users.noreply.github.com>
2020-08-20 19:20:55 +0530

    addition

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
+def addition(number_1,number_2):
+    sum=number_1+number_2
+    print("Sum of",number_1,"and",number_2,"is:",sum)
+
+
 def get_input():
     number_1=int(input("Enter the first number: "))
     number_2=int(input("Enter the second number: "))
