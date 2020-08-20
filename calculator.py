6a9ff34a5a6cc09d70c12de7c2b8be34950e72ec
Abu-thahir <69951869+Abu-thahir@users.noreply.github.com>
2020-08-20 21:56:32 +0530

    Updated Calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,28 @@
 def addition(number_1, number_2):
+  return number_1+number_2
 
 def subtraction(number_1, number_2):
+  return number_1-number_2
 
 def multiplication(number_1, number_2):
+   return number_1*number_2
 
 def division(number_1, number_2):
+   return number_1/number_2
 
 def get_input():
+    global x,y,op     
+    x = int(input("Enter num1 :"))
+    op = input("Operator (+, -, *, /):")
+    y = int(input("Enter num2 :"))
 
-input()
+get_input()
+
+if op == "+":
+    print("Answer : ",addition(x,y))
+elif op == "-":
+    print("Answer : ",subtraction(x,y))
+elif op == "*":
+    print("Answer : ",multiplication(x,y))
+elif op == "/":
+    print("Answer : ",division(x,y))
