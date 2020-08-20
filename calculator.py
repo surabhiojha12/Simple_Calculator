c9b01e1576e21f9d319662fa32236a8d6261b615
suganthiM2306 <69864460+suganthiM2306@users.noreply.github.com>
2020-08-20 14:42:35 +0530

    choosing operation

--- calculator.py
+++ calculator.py
@@ -10,3 +10,20 @@ print("SIMPLE CALCULATOR")
 number_1=int(input("Enter First Number"))
 number_2=int(input("Enter Second Number")) 
 
+
+print("Select option: 1.Addition \n 2.Subtraction \n 3.Multiplication \n 4.Division")
+
+option=int(input("your option : "))
+
+if(option==1):
+    
+elif(option==2):
+
+elif(option==3):
+
+elif(option==4):
+    
+else:
+    print("Invalid Input")
+
+
