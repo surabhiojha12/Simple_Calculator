55ac9727eb5c5aa0eb73b101de911480950b0a4f
Ancytera <69912080+Ancytera@users.noreply.github.com>
2020-08-20 09:59:07 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -7,5 +7,11 @@ def multiplication(number_1, number_2):
 def division(number_1, number_2):
 
 def get_input():
+	print("....SIMPLE CALCULATOR...")
+	print("Select Operation\n1.Addition \n2.Substraction \n3.Multiplication \n 4.Division")
+	number1=int(input("Enter the first number="))
+	number2=int(input("Enter the second number="))
+    option=int(input("Operation:"))
 
-input()
+input():
+	get_input()
