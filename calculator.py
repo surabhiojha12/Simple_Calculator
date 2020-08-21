a7c75845f921f0b0263ce06479f2520b259ae5b3
Alagu Gomathi Brindha <69798418+BrindhaNainar@users.noreply.github.com>
2020-08-21 17:49:08 +0530

    Update calculator.py
    filled the function body

--- calculator.py
+++ calculator.py
@@ -1,11 +1,22 @@
 def addition(number_1, number_2):
+  print(a+b)
 
 def subtraction(number_1, number_2):
+  print(a-b)
 
 def multiplication(number_1, number_2):
+  print(a*b)
 
 def division(number_1, number_2):
+  print(a/b)
 
 def get_input():
+  a = int(input())
+  b = int(input())
 
 input()
+
+addition(a,b)
+subtraction(a,b)
+multiplication(a,b)
+division(a,b)
