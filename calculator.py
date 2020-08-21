d5b7f29f1396601fd242ce4381d39accb93b46c9
Vijay0710 <vijaymangalani4588@gmail.com>
2020-08-21 17:04:33 +0530

    Modified Simple Calculator

--- calculator.py
+++ calculator.py
@@ -1,11 +1,21 @@
 def addition(number_1, number_2):
+    return number_1+number_2
 
 def subtraction(number_1, number_2):
+    return number_1-number_2
 
 def multiplication(number_1, number_2):
+    return number_1*number_2
 
 def division(number_1, number_2):
+    return number_1/number_2
 
 def get_input():
+    m,n=input().split()
+    print(addition(int(m),int(n)))
+    print(subtraction(int(m),int(n)))
+    print(multiplication(int(m),int(n)))
+    print(division(int(m),int(n)))
 
-input()
+get_input()
+    
