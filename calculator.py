696dfa3c8483f71826ea364a84864cb104c40d85
SuryaV-02 <suryaedu71@gmail.com>
2020-08-21 22:10:15 +0530

    Modified1

--- calculator.py
+++ calculator.py
@@ -7,14 +7,17 @@ def multiplication(number_1, number_2):
     return number_1*number_2
 
 def division(number_1, number_2):
-    return number_1/number_2
+    if(number_2==0):
+        return "zero division error"
+    else:
+        return number_1/number_2
 
 def get_input():
-    list=[]
-    list=input().split()
-    print(addition(int(list[0]),int(list[1])))
-    print(subtraction(int(list[0]),int(list[1])))
-    print(multiplication(int(list[0]),int(list[1])))
-    input(division(int(list[0]),int(list[1])))
+    number_1=int(input("Enter 1st number :: "))
+    number_2=int(input("Enter second number :: "))
+    print("Addition :: "+str(addition(number_1,number_2)))
+    print("Subtraction :: "+str(subtraction(number_1,number_2)))
+    print("Multiplication :: "+str(multiplication(number_1,number_2)))
+    print("Division :: "+str(division(number_1,number_2)))
     
 get_input()
