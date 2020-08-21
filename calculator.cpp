8b6dfe6c189fc4a9c8b9713aad8f808e92a6c3ae
charann01 <61188435+charann01@users.noreply.github.com>
2020-08-21 20:37:12 +0530

    completed multiplication function

--- calculator.cpp
+++ calculator.cpp
@@ -10,7 +10,7 @@ int subtraction(int number_1, int number_2){
 }
 
 int multiplication(int number_1, int number_2){
-
+    return number_1 * number_2;
 }
 
 int division(int number_1, int number_2){
