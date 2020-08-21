e9fad58fb6fda792b855c8b59b83d6b2f8ec1fa8
qor-101 <qor6119@gmail.com>
2020-08-21 22:40:53 +0530

    FInal calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -9,7 +9,7 @@ int subtraction(int number_1, int number_2){
 return number_1 - number_2;
 }
 
-int multiplication(int number_1, int number_2){
+int multiplication (int number_1, int number_2){
 return number_1*number_2;
 }
 
