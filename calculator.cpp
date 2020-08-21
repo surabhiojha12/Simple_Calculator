788eac8f44362d9fe8e60f7d68fbe84735f272d8
Mithra1909 <69853998+Mithra1909@users.noreply.github.com>
2020-08-21 19:45:54 +0530

    updated multiplication

--- calculator.cpp
+++ calculator.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int addition(int number_1, int number_2)
 {
-cout<<number_1+number;
+cout<<number_1+number_2;
 }
 
 int subtraction(int number_1, int number_2){
-cout<<number_1-number;
+cout<<number_1-number_2;
 }
 
 int multiplication(int number_1, int number_2){
-
+cout<<number_1*number_2;
 }
 
 int division(int number_1, int number_2){
