6e5cb2657cdc764789b2764244cce0ceb3f2ce47
k-dhiksha <dhiksha.k.2018.cse@rajalakshmi.edu.in>
2020-08-22 00:03:22 +0530

    addition

--- calculator.py
+++ calculator.py
@@ -1,10 +1,15 @@
 def addition(number_1, number_2):
-    
+    return(number_1+number_1)
 def subtraction(number_1, number_2):
+
 
 def multiplication(number_1, number_2):
 
+
+    
 def division(number_1, number_2):
+
+
 
 def get_input():
     number_1 = int(input())
