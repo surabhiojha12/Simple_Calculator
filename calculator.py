d9df4b28ebc012588242bfe1b3de98cee7f6e343
Viknesh <vikneshwar.gk.2018.cse@rajalakshmi.edu.in>
2020-08-21 14:25:49 +0530

    Added Input functionality and Body of the code

--- calculator.py
+++ calculator.py
@@ -16,7 +16,26 @@ def division(number_1, number_2):
     return div
 
 def get_input():
+    num1 = int(input("Enter 1st number : "))
+    num2 = int(input("Enter 2nd number : "))
+    return num1, num2
 
-input()
-
-
+while(1):
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+    ch = int(input("Enter Choice"))
+    if(ch == 5):
+        break
+    elif(ch < 5):
+        num1, num2 = get_input()
+        if(ch == 1):
+            print("Sum : ", addition(num1, num2))
+        elif(ch == 2):
+            print("Differnce : ", subtraction(num1, num2))
+        elif(ch == 3):
+            print("Multiplication : ", multiplication(num1, num2))
+        elif(ch == 4):
+            print("Division : ", division(num1, num2))
