921e96eafc2b0a58522bb5acc32b8455df07e909
Roshiniv11 <69917451+Roshiniv11@users.noreply.github.com>
2020-08-21 14:26:21 +0000

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -19,7 +19,20 @@ int division(int number_1, int number_2){
 }
 
 int get_input(){
-
+ int number_1,number_2;
+  char op;
+  cout<<"enter operation";
+  cin>>op;
+ switch(op)
+ {
+    case '+': addition(number_1,number_2)
+      break;
+    case '-': subtraction(number_1,number_2)
+       break;
+    case '*': multiplication(number_1,number_2)
+       break;
+    case '/': division(number_1,number_2)
+       break;
 }
 
 int main(){
