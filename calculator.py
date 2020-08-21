0e7130baa79aaeee0fd7cf2e75843c23d8e5c527
Viknesh <vikneshwar.gk.2018.cse@rajalakshmi.edu.in>
2020-08-21 13:57:44 +0530

    Added addition function

--- calculator.py
+++ calculator.py
@@ -1,11 +1,17 @@
 def addition(number_1, number_2):
+    sum = number_1 + number_2
+    return sum
 
 def subtraction(number_1, number_2):
+    
 
 def multiplication(number_1, number_2):
+    
 
 def division(number_1, number_2):
 
 def get_input():
 
 input()
+
+
