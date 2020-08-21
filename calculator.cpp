ffd2f0edf1fe9ff4526fc373cd107d4f3a974e16
charann01 <61188435+charann01@users.noreply.github.com>
2020-08-21 20:40:57 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -18,7 +18,17 @@ int division(int number_1, int number_2){
 }
 
 int get_input(){
-
+    int a,b;
+    cout<<"Enter a and b"<<endl;
+    cin>>a>>b;
+    cout<<"Sum:";
+    cout<<addition(a,b)<<endl;
+    cout<<"Differnence:";
+    cout<<subtraction(a,b)<<endl;
+    cout<<"Product:";
+    cout<<multiplication(a,b)<<endl;
+    cout<<"Division:";
+    cout<<division(a,b)<<endl;
 }
 
 int main(){
