d5d375ff561fb91cfbebdc7d5074f6bf497790fc
suraj6501 <67002146+suraj6501@users.noreply.github.com>
2020-08-21 16:28:18 +0530

    Update calculator.py
    Updated addition function definition.

--- calculator.py
+++ calculator.py
@@ -1,11 +1,12 @@
 def addition(number_1, number_2):
-
+    int Sum = number_1 + number_2
+    print("Sum of " + number_1 + " and " + number_2 +" is " + Sum)
 def subtraction(number_1, number_2):
-
+    
 def multiplication(number_1, number_2):
-
+    
 def division(number_1, number_2):
-
+    
 def get_input():
 
-input()
+input()input()
