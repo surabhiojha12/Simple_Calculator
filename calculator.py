ab562f0510ef8a9ea0fe9bd2fd4440f5778d59f6
Sidharthraj14 <69853862+Sidharthraj14@users.noreply.github.com>
2020-08-21 19:26:56 +0530

    Update calculator.py
    updated multiplication

--- calculator.py
+++ calculator.py
@@ -4,8 +4,8 @@ def addition(number_1, number_2):
 def subtraction(number_1, number_2):
   return number_1 - number_2;
   
-
 def multiplication(number_1, number_2):
+  return number_1 * number_2;
   
 
 def division(number_1, number_2):
