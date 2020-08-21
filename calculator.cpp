c8d2ee32033c10a01da61f5e0a6d7ec82a948eb3
charann01 <61188435+charann01@users.noreply.github.com>
2020-08-21 21:28:49 +0530

    Variable name changed and added exception for 'division by zero'

--- calculator.cpp
+++ calculator.cpp
@@ -14,21 +14,24 @@ int multiplication(int number_1, int number_2){
 }
 
 int division(int number_1, int number_2){
-    return number_1 / number_2;
+    if(number_2 != 0)
+        return number_1 / number_2;
+     else
+          cout <<"Division by zero is not possible"<<endl; 
 }
 
 int get_input(){
-    int a,b;
+    int number_1,number_2;
     cout<<"Enter a and b"<<endl;
-    cin>>a>>b;
+    cin>>number_1>>number_2;
     cout<<"Sum:";
-    cout<<addition(a,b)<<endl;
+    cout<<addition(number_1,number_2)<<endl;
     cout<<"Differnence:";
-    cout<<subtraction(a,b)<<endl;
+    cout<<subtraction(number_1,number_2)<<endl;
     cout<<"Product:";
-    cout<<multiplication(a,b)<<endl;
+    cout<<multiplication(number_1,number_2)<<endl;
     cout<<"Division:";
-    cout<<division(a,b)<<endl;
+    cout<<division(number_1,number_2)<<endl;
 }
 
 int main(){
