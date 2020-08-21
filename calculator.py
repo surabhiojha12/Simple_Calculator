62b25f697eb42379578fdcb539c631d0bcca444b
JAYASHREE-DEV <jayashree.d.2018.cse@rajalakshmi.edu.in>
2020-08-22 00:06:35 +0530

    multiplication_operation

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ def addition(number_1, number_2):
 def subtraction(number_1, number_2):
     return(number_1 - number_2)
 def multiplication(number_1, number_2):
-
+    return(number_1 * number_2)
 def division(number_1, number_2):
 
 def get_input():
@@ -15,3 +15,4 @@ def get_input():
     number_1, number_2 = get_input()   
     print(addition(number_1, number_2))
     print(subtraction(number_1, number_2))
+    print(multiplication(number_1, number_2))
