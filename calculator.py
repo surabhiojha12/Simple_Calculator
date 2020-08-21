cc7162605f52cfca42a3ee5a9c284ce9b098624e
daadreyaa30 <69893232+daadreyaa30@users.noreply.github.com>
2020-08-21 20:59:28 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -8,11 +8,28 @@ def multiplication(number_1, number_2):
   return number_1 * number_2
 
 def division(number_1, number_2):
-  return nuber_1 / number_2
-
+  try:
+    return nuber_1 / number_2
+  except:
+    return 'cannot divide'
+  
 def get_input():
-  number_1 = int(input())
-  number_2 = int(input())
+  data = input()
+  if '+' in data:
+    number_1, number_2 = [data.split('+')]
+    print(addition(number_1, number_2)
+  elif '-' in data:
+    number_1, number_2 = [data.split('-')]
+    print(addition(number_1, number_2)
+  elif '*' in data:
+    number_1, number_2 = [data.split('*')]
+    print(addition(number_1, number_2)
+  elif '/' in data:
+    number_1, number_2 = [data.split('/')]
+    print(addition(number_1, number_2)'
+  else:
+    print("wrong input")
+          
  
 get_input()
 
