960c5ef952b5eba6752b961b9c8acb38f61f8bf6
Vijay0710 <vijaymangalani4588@gmail.com>
2020-08-21 22:10:11 +0530

    Modified (1) Simple Calculator

--- calculator.py
+++ calculator.py
@@ -8,14 +8,18 @@ def multiplication(number_1, number_2):
     return number_1*number_2
 
 def division(number_1, number_2):
+    if number_2 == 0:
+        return("Zero Division Error")
     return number_1/number_2
 
 def get_input():
     m,n=input().split()
-    print(addition(int(m),int(n)))
-    print(subtraction(int(m),int(n)))
-    print(multiplication(int(m),int(n)))
-    print(division(int(m),int(n)))
+    number1 = int(m)
+    number2 = int(n)
+    print("Addition is: ",addition(number1,number2))
+    print("Subtraction is: ",subtraction(number1,number2))
+    print("Multiplication is: ",multiplication(number1,number2))
+    print("Division is: ",division(number1,number2))
 
 get_input()
     
