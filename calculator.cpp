43f8634ba561255de7c6e561c9a569adab3a7f5b
Roshiniv11 <69917451+Roshiniv11@users.noreply.github.com>
2020-08-21 14:16:22 +0000

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 int addition(int number_1, int number_2){
-int add=number_1+number_2;
-     cout<<add;
+   cout<<number_1+number_2;
 }
 
 int subtraction(int number_1, int number_2){
@@ -12,11 +11,11 @@ int subtraction(int number_1, int number_2){
 }
 
 int multiplication(int number_1, int number_2){
-
+     cout<<number_1*number_2;
 }
 
 int division(int number_1, int number_2){
-
+     cout<<number_1/number_2;
 }
 
 int get_input(){
