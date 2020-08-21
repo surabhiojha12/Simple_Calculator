d94b11ba6fcce914f9c98f8fb240c9b8ae13d7dd
k-dhiksha <dhiksha.k.2018.cse@rajalakshmi.edu.in>
2020-08-22 00:06:18 +0530

    subtraction

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
 def addition(number_1, number_2):
-    return(number_1+number_1)
+    return(number_1+number_2)
 def subtraction(number_1, number_2):
-
+    return(number_1-number_2)
 
 def multiplication(number_1, number_2):
 
 
-    
+
 def division(number_1, number_2):
 
 
