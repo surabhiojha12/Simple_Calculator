777c4d25b00f924a005f314603483b27aadb82c2
AshwinHPrasad <ashwinprasad202@gmail.com>
2020-08-21 21:32:48 +0530

    division_by_Zero
    added a new conditional statement for division by zero case

--- calculator.cpp
+++ calculator.cpp
@@ -14,8 +14,14 @@ int multiplication(int number_1, int number_2){
 }
 
 int division(int number_1, int number_2){
-    return number_1/number_2;
-}
+    if(number_2 == ){
+        cout<<" Division by 0 is not defined "<<end;
+        return 0;
+    }
+    else{
+        return number_1/number_2;
+    }
+}    
 
 int get_input(){
     int number_1,number_2;
