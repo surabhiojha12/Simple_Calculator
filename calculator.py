6ab12b4d7ab2197126620e192262b12093836222
SuryaV-02 <suryaedu71@gmail.com>
2020-08-21 17:09:04 +0530

    ModifiedSimpleCalculator

--- calculator.py
+++ calculator.py
@@ -1,11 +1,20 @@
 def addition(number_1, number_2):
-
+    return number_1+number_2
 def subtraction(number_1, number_2):
+    return number_1-number_2
 
 def multiplication(number_1, number_2):
+    return number_1*number_2
 
 def division(number_1, number_2):
+    return number_1/number_2
 
 def get_input():
-
-input()
+    list=[]
+    list=input().split()
+    print(addition(int(list[0]),int(list[1])))
+    print(subtraction(int(list[0]),int(list[1])))
+    print(multiplication(int(list[0]),int(list[1])))
+    input(division(int(list[0]),int(list[1])))
+    
+get_input()
