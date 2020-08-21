9619b1252d01e10709286096a3e3f62dfa20bc3f
Mithra1909 <69853998+Mithra1909@users.noreply.github.com>
2020-08-21 19:54:58 +0530

    updated input()

--- calculator.cpp
+++ calculator.cpp
@@ -18,8 +18,21 @@ int division(int number_1, int number_2){
 cout<<number_1/number_2;
 }
 
-int get_input(){
-
+int get_input(){ int input1,input2; char choice;
+cin<<input1;
+cin<<input2;
+cin<<choice;
+                switch(choice)
+                { case '+': addition(input1,input2);
+                            break;
+                 case '-': subtraction(input1, input2);
+                            break;
+                 case '*': multiplication(input1, input2);
+                            break;
+                 case '/': division(input1,input2);
+                            break;
+                 default: cout<<"ERROR";
+                }
 }
 
 int main(){
