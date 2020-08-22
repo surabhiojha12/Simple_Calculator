b38c257dfd8f71dd047b12cdc00a904d30885851
AshwinHPrasad <ashwinprasad202@gmail.com>
2020-08-22 10:51:33 +0530

    missing_zero

--- calculator.cpp
+++ calculator.cpp
@@ -14,7 +14,7 @@ int multiplication(int number_1, int number_2){
 }
 
 int division(int number_1, int number_2){
-    if(number_2 == ){
+    if(number_2 == 0){
         cout<<" Division by 0 is not defined "<<end;
         return 0;
     }
