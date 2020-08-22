16db7a2e6731054d5f1c6fc3a760a25ba06e715c
suraj6501 <67002146+suraj6501@users.noreply.github.com>
2020-08-22 09:21:25 +0530

    Update calculator.py
    Updated division function

--- calculator.py
+++ calculator.py
@@ -11,8 +11,13 @@ def multiplication(number_1, number_2):
     print("Multiplication of " + number_1 + " and " + number_2 +" is " + Mul)
     
 def division(number_1, number_2):
+    if number_2 != 0 :
+        int Div = number_1 / number_2
+        print("Division of " + number_1 + " and " + number_2 +" is " + Div)
+    else :
+        print("Divisor can't be 0 \n")
     
 
 def get_input():
    
-
+ 
