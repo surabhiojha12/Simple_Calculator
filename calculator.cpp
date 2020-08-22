5d85e441f0f04187200da5654db27fc6de0c95e4
Surendhar <65447707+surendhar-code@users.noreply.github.com>
2020-08-22 12:01:51 +0530

    Fixed errors in mul and div function

--- calculator.cpp
+++ calculator.cpp
@@ -14,14 +14,14 @@ int subtraction(int number_1, int number_2){
 }
 
 int multiplication(int number_1, int number_2){
-    number_1*number_2;
+   
     return number_1*number_2;
 
 }
 
 int division(int number_1, int number_2){
     if(number_2==0)
-        cout<<"Not defined"<<endl;
+        return 0;
     else
         return number_1/number_2;
 
